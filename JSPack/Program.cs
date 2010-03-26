c7b375d8ef6c4f4824d82065e6512f240bb36be4
Chad Burggraf <chad.burggraf@gmail.com>
2010-03-25 22:42:28 -0600

    Added a timestamp to the packing output for watch scenarios.

--- JSPack/Program.cs
+++ JSPack/Program.cs
@@ -17,7 +17,7 @@ namespace JSPack
         #region Constants
 
         private const string CHANGES = "Changes detected in source directory, re-packaging.";
-        private const string TIME = "Packing completed in {0:N2} seconds.\n";
+        private const string TIME = "Packing completed in {0:N2} seconds at {1:F}.\n";
         private const string USAGE = "Usage: jspack /map:path_to_map [/src:source_dir /target:target_dir /version:version_number /actions:true|false]\n";
         private const string WATCHING = "Watching for changes. Press Ctl+C to quit.\n";
         
@@ -41,7 +41,7 @@ namespace JSPack
                         DateTime start = DateTime.Now;
                         Packer = new Packer(loader, mapArgs);
                         Packer.Pack(Console.Out, Console.Error);
-                        Console.WriteLine(TIME, DateTime.Now.Subtract(start).TotalSeconds);
+                        Console.WriteLine(TIME, DateTime.Now.Subtract(start).TotalSeconds, DateTime.Now);
 
                         if (!String.IsNullOrEmpty(inputArgs["watch"]) && Convert.ToBoolean(inputArgs["watch"]))
                         {
@@ -107,7 +107,7 @@ namespace JSPack
                         Console.WriteLine(CHANGES);
                         DateTime start = DateTime.Now;
                         Packer.Pack(Console.Out, Console.Error);
-                        Console.WriteLine(TIME, DateTime.Now.Subtract(start).TotalSeconds);
+                        Console.WriteLine(TIME, DateTime.Now.Subtract(start).TotalSeconds, DateTime.Now);
                         Console.WriteLine(WATCHING);
                     };
 
